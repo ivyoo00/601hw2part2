1c46643c0b0bbef00a71fe9cddd71b6951993a77
Jiali Ge <ivydany@bu.edu>
2017-10-10 01:41:35 -0400

    Update index.js

--- cloud-functions-start/functions/index.js
+++ cloud-functions-start/functions/index.js
@@ -14,7 +14,11 @@
  * limitations under the License.
  */
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+// Import the Firebase SDK for Google Cloud Functions.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
 
